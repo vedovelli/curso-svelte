392f8a51488976d3397a63a2c960cd810053afd8
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-13 13:06:51 +0200

    Update App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  const clickHandler = () => {};
+</script>
+
+<main class="container mx-auto pt-6">
+  <div>
+    <button
+      on:click={clickHandler}
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+      rounded">
+      Click me
+    </button>
+  </div>
+  <p>count: {count}</p>
+</main>
