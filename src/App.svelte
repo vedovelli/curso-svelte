e094be62e58f9d55944a0b2bdb27a82b2ff1f4df
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-13 17:13:27 +0200

    Basic markup

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Card from "./components/Card.svelte";
+</script>
+
+<div class="container mx-auto mt-4">
+  <Card />
+</div>
---
+++ src/components/Card.svelte
@@ -0,0 +1,31 @@
+<div class="w-full max-w-sm lg:max-w-full lg:flex">
+  <div
+    class="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-auto lg:w-48 lg:rounded-t-none lg:rounded-l"
+    style="background-image: url('/image2.jpg')"
+    title="Woman holding a mug" />
+  <div
+    class="flex flex-col justify-between p-4 leading-normal bg-white border-b border-l border-r border-gray-400 rounded-b lg:border-l-0 lg:border-t lg:border-gray-400 lg:rounded-b-none lg:rounded-r">
+    <div class="mb-8">
+      <p class="flex items-center text-sm text-gray-600">
+        <svg
+          class="w-3 h-3 mr-2 text-gray-500 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20">
+          <path
+            d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0
+            1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7
+            6v2h6V6a3 3 0 0 0-6 0z" />
+        </svg>
+        Members only
+      </p>
+      <div class="mb-2 text-xl font-bold text-gray-900">
+        Can coffee make you a better developer?
+      </div>
+      <p class="text-base text-gray-700">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+        nihil.
+      </p>
+    </div>
+  </div>
+</div>
