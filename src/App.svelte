5cbb5e89086b42be8522185bdbf08252a1820acf
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-14 10:03:25 +0200

    Update App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let images = [
+    {
+      path: "/image1.jpg",
+      description: "Primeira imagem"
+    },
+    {
+      path: "/image2.jpg",
+      description: "Segunda imagem"
+    }
+  ];
+</script>
+
+<img />
+<img />
