37280feedd99825079e9669b764c160e6640ebdd
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-14 10:08:03 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,9 @@
       description: "Segunda imagem"
     }
   ];
+
+  const [imgObj1, imgObj2] = images;
 </script>
 
-<img />
-<img />
+<img src={imgObj1.path} alt={imgObj1.description} />
+<img src={imgObj2.path} alt={imgObj2.description} />
