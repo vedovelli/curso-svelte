c4972ffda73b901e3e139876c52e475e17d42309
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-14 09:46:34 +0200

    Finished

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+    adipisci molestiae, error eligendi et, quo sint optio quam voluptas nulla at
+    fugiat facere dolores aut nisi est officia illum. Aliquid.
+  </p>
+</main>
