0b24f5b640f06e582a803fad5bd65ff4dd98020d
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-15 09:40:00 +0200

    Finished

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Card from "./components/Card.svelte";
 
   let list = [
-    { title: "Primeiro card", cover: "/image2.jpg" },
+    { title: "Primeiro card", membersOnly: true, cover: "/image2.jpg" },
     { title: "Segundo card", membersOnly: true, cover: "/image2.jpg" },
     { title: "Terceiro card", cover: "/image1.jpg" },
     { title: "Quarto card", membersOnly: true, cover: "/image2.jpg" },
@@ -13,6 +13,8 @@
 
 <div class="container mx-auto mt-4">
   <div class="grid grid-cols-2 gap-4">
-    <Card />
+    {#each list as item}
+      <Card {...item} />
+    {/each}
   </div>
 </div>
