78102d772fe7c02cf6e5bd498e3e10ce0cd1e417
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-18 11:57:57 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,36 @@
 <script>
+  import { onMount } from "svelte";
   import PrettyPrint from "./PrettyPrint.svelte";
 
-  let formData = {};
+  let button;
+
+  let formData = {
+    email: "",
+    select: "",
+    range: 0,
+    radio: "",
+    newsletter: true,
+    terms: false
+  };
+
+  const submit = () => {
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.disabled = false;
+      alert(
+        `Estes dados serão enviados para a API: ${JSON.stringify(formData)}`
+      );
+    }, 1000);
+  };
 </script>
 
 <div class="container">
-  <form>
+  <form on:submit|preventDefault={submit}>
     <div class="form-group">
       <label for="exampleInputEmail1">Email address</label>
       <input
+        bind:value={formData.email}
         type="email"
         class="form-control"
         id="exampleInputEmail1"
@@ -19,7 +41,10 @@
     </div>
     <div class="form-group">
       <label for="exampleFormControlSelect1">Example select</label>
-      <select class="form-control" id="exampleFormControlSelect1">
+      <select
+        bind:value={formData.select}
+        class="form-control"
+        id="exampleFormControlSelect1">
         <option>1</option>
         <option>2</option>
         <option>3</option>
@@ -29,12 +54,17 @@
     </div>
     <div class="form-group">
       <label for="formControlRange">Example Range input</label>
-      <input type="range" class="form-control-range" id="formControlRange" />
+      <input
+        bind:value={formData.range}
+        type="range"
+        class="form-control-range"
+        id="formControlRange" />
     </div>
     <div class="form-check">
       <input
         class="form-check-input"
         type="radio"
+        bind:group={formData.radio}
         name="exampleRadios"
         id="exampleRadios1"
         value="option1"
@@ -45,6 +75,7 @@
       <input
         class="form-check-input"
         type="radio"
+        bind:group={formData.radio}
         name="exampleRadios"
         id="exampleRadios2"
         value="option2" />
@@ -53,10 +84,28 @@
       </label>
     </div>
     <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <input
+        bind:checked={formData.newsletter}
+        type="checkbox"
+        class="form-check-input"
+        id="exampleCheck1" />
+      <label class="form-check-label" for="exampleCheck1">
+        Assinar a newsletter?
+      </label>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="form-group form-check">
+      <input
+        bind:checked={formData.terms}
+        type="checkbox"
+        class="form-check-input"
+        id="exampleCheck2" />
+      <label class="form-check-label" for="exampleCheck2">
+        Aceito os termos de uso
+      </label>
+    </div>
+    <button bind:this={button} type="submit" class="btn btn-primary">
+      Submit
+    </button>
   </form>
 
   <PrettyPrint {formData} />
