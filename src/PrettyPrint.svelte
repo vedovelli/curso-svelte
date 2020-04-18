fcdf9fc127ad8b9eafe4935c5c41678e1bccd827
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-18 12:27:47 +0200

    Update PrettyPrint.svelte

--- src/PrettyPrint.svelte
+++ src/PrettyPrint.svelte
@@ -1,7 +1,23 @@
 <script>
+  import { onMount, beforeUpdate, afterUpdate } from "svelte";
   import prettyPrintJson from "pretty-print-json";
 
   export let formData = {};
+
+  onMount(() => {
+    // eslint-disable-next-line
+    console.log("Pronto para ser usado");
+  });
+
+  beforeUpdate(() => {
+    // eslint-disable-next-line
+    console.log("Receberei atualização");
+  });
+
+  afterUpdate(() => {
+    // eslint-disable-next-line
+    console.log("Fui atualizado");
+  });
 </script>
 
 <div class="output">
