e5a34372bbfbb044b3a3c2185fecce28e45dc3e1
Fabio Vedovelli <vedovelli@gmail.com>
2020-04-30 11:00:01 +0200

    Finished

--- src/features/Movies/MovieList.svelte
+++ src/features/Movies/MovieList.svelte
@@ -1,6 +1,11 @@
 <script>
+  import { fly } from "svelte/transition";
   import { store, movies } from "../../store";
   import MovieCard from "./MovieCard.svelte";
+
+  let current = null;
+
+  const setCurrent = id => (current === id ? (current = null) : (current = id));
 </script>
 
 <div class="mb-16">
@@ -9,10 +14,16 @@
       <h3 class="py-6 mt-3 text-center shadow sm:rounded-lg xl:mx-3 bg-gray-50">
         Nenhum filme encontrado
       </h3>
-    {:else}
-      <ul class="m-0 xl:grid xl:grid-cols-2">
+    {/if}
+    {#if $movies.length}
+      <ul
+        class="m-0 xl:grid xl:grid-cols-2"
+        in:fly={{ y: -20, duration: 400 }}
+        out:fly={{ y: 20, duration: 400 }}>
         {#each $movies as movie}
           <li
+            on:click={setCurrent(movie.id)}
+            class:bg-red-200={current === movie.id}
             class="p-1 mx-0 my-3 bg-white shadow cursor-pointer xl:mx-3
             sm:rounded-lg xl:justify-between">
             <MovieCard {movie} />
